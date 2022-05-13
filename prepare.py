e37bd325a7ba937bb7d47beb06b25fc10d0039ad
Ben Smith <ben.f.smith07@gmail.com>
2022-05-13 11:13:51 -0500

    added train/validate/test split to prepare.py

--- prepare.py
+++ prepare.py
@@ -5,6 +5,8 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 import pandas as pd
+from sklearn.model_selection import train_test_split
+random_state = 42
 
 def basic_clean(text):
     text = text.lower()
@@ -50,3 +52,35 @@ def prep_repos(df):
     df = df.rename(columns={'readme_contents':'original'})
     df = nlp_prep(df)
     return df
+
+def train_validate_test_split(x, y, test_size=.2, validate_size=.3, random_state=random_state):
+    '''
+    This function takes in x and y, then splits each of them into three separate samples: 
+    train, test, and validate, for use in machine learning modeling.
+
+    The samples are returned in the following order: 
+         x_train, y_train, x_validate, y_validate, x_test, y_test
+    
+    The function also prints the size of each sample.
+    '''
+    # split x into train and test
+    x_train, x_test = train_test_split(x, test_size=test_size, random_state=random_state)
+    # further split the x_train into train and validate
+    x_train, x_validate = train_test_split(x_train, test_size=validate_size, random_state=random_state)
+
+    # split y into train and test
+    y_train, y_test = train_test_split(y, test_size=test_size, random_state=random_state)
+    # further split the y_train into train and validate
+    y_train, y_validate = train_test_split(y_train, test_size=validate_size, random_state=random_state)
+
+
+    # print the size of each resulting sample
+    print(f'train\t n = {x_train.shape[0]}')
+    print(f'train\t n = {y_train.shape[0]}')
+    print(f'validate n = {x_validate.shape[0]}')
+    print(f'train\t n = {y_validate.shape[0]}')
+    print(f'test\t n = {x_test.shape[0]}')
+    print(f'train\t n = {y_test.shape[0]}')
+    
+
+    return x_train, y_train, x_validate, y_validate, x_test, y_test
